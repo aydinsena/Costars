d861f1eafe498451ad350fd6b567d6ca2d7c9c76
Sena Aydin <senaaydin484@gmail.com>
2023-11-20 12:33:17 +0100

    fix file names because it's confusing

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,8 +1,8 @@
 module.exports = (req, res, next) => {
-  // checks if the user is logged in when trying to access a specific page
-  if (!req.session.currentUser) {
-    return res.redirect("/auth/login");
+  // if an already logged in user tries to access the login page it
+  // redirects the user to the home page
+  if (req.session.currentUser) {
+    return res.redirect("/user/dashboard");
   }
-
   next();
 };
