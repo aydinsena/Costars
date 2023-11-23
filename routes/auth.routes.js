197d68aa3a61ca4d9561a3dc45f218c4b1c2b0bf
Gizem <109289446+gizemayegul@users.noreply.github.com>
2023-11-23 15:19:35 +0100

    added bootstrap and solved login nav-bar issue

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -120,7 +120,6 @@ router.post("/login", isLoggedOut, (req, res, next) => {
     })
     .catch((err) => next(err));
 });
-
 router.get("/logout", isLoggedIn, (req, res) => {
   req.session.destroy((err) => {
     res.clearCookie("connect.sid");
@@ -128,7 +127,6 @@ router.get("/logout", isLoggedIn, (req, res) => {
       res.status(500).render("/auth/logout", { errorMessage: err.message });
       return;
     }
-
     res.redirect("/");
   });
 });
